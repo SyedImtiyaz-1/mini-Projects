1f97c95c95062914c448d7bda27b1de6a2684562
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-12 19:11:03 +0530

    updated

--- Toggle-Navbar/script.js
+++ Toggle-Navbar/script.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById('click-nav');
 let ul_hide = document.querySelector('ul');
+let li_hide = document.querySelector('li-nav');
 
 btn.addEventListener("click", function(){
    ul_hide.classList.toggle("active");
+   li_hide.classList.toggle("active");
 })
