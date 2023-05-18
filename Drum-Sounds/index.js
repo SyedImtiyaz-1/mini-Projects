e6cd8b31ab6d6347c41e40cbda3b4909ca2120f7
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-18 14:52:15 +0530

    added @media queries

---
+++ Drum-Sounds/index.js
@@ -0,0 +1,10 @@
+var drum_index = document.querySelectorAll(".drum").length; //because counting length;
+
+let drum_value = document.querySelectorAll(".drum"); // it's the value because it's not counting anything
+
+// loop to click every single button
+for (var i = 0; i < drum_index; i++) {
+    drum_value[i].addEventListener("click", function () {
+        alert("hi");
+    });
+}
