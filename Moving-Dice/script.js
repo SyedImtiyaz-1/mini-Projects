f9fad81f2f00d25091b839dd0788097e02ddf5a3
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-18 13:36:51 +0530

    done

--- Moving-Dice/script.js
+++ Moving-Dice/script.js
@@ -1,3 +1,5 @@
+var count1 = 1; 
+var count2 = 1;
 const btn = document.querySelector("button");
 btn.addEventListener("click", function () {
 let randomCount1 = Math.floor(Math.random() * 6) + 1;
@@ -6,16 +8,26 @@ let imageSource1 = "images/" + takeImage;
 let img1 = document.querySelectorAll("img")[0];
 img1.setAttribute("src", imageSource1);
 
+// loop to count
+
 let randomCount2 = Math.floor(Math.random() * 6) + 1;
 let imageSource2 = "images/dice" + randomCount2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
 if (randomCount1 > randomCount2) {
   document.querySelector("h2").innerHTML = "Player 1 Wins";
+  document.querySelector("h3 span").innerHTML = `(${count1++})`;
+  
 }
 else if (randomCount1 < randomCount2) {
   document.querySelector("h2").innerHTML = "Player 2 Wins";
+  document.querySelector("#count2").innerHTML = `(${count2++})`;
 }
-else{
+else if (randomCount1 === randomCount2){
     document.querySelector("h2").innerHTML = "Tie";
 }
+
+else{
+  alert("Refresh the page there is a error");
+}
+
 });
