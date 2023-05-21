7b7462a208af881d4b0e601ca79f26dbd8747aa9
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-21 11:52:26 +0530

    added sound for moving dice

--- Moving-Dice/script.js
+++ Moving-Dice/script.js
@@ -1,5 +1,6 @@
 var count1 = 1;
 var count2 = 1;
+
 const btn = document.querySelector("button");
 btn.addEventListener("click", function () {
   let randomCount1 = Math.floor(Math.random() * 6) + 1;
@@ -8,20 +9,22 @@ btn.addEventListener("click", function () {
   let img1 = document.querySelectorAll("img")[0];
   img1.setAttribute("src", imageSource1);
 
-  // loop to count
-
+let clickSound = new Audio('./sound/diceSound.mp3')
+clickSound.play();
+  
   let randomCount2 = Math.floor(Math.random() * 6) + 1;
   let imageSource2 = "images/dice" + randomCount2 + ".png";
   document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
   if (randomCount1 > randomCount2) {
     document.querySelector("h2").innerHTML = "Player 1 Wins";
-    document.querySelector("h3 span").innerHTML = `(${count1++})`;
+    document.querySelector("h3 span").innerHTML = `(Points: ${count1++})`;
   } else if (randomCount1 < randomCount2) {
     document.querySelector("h2").innerHTML = "Player 2 Wins";
-    document.querySelector("#count2").innerHTML = `(${count2++})`;
+    document.querySelector("#count2").innerHTML = `(Points: ${count2++})`;
   } else if (randomCount1 === randomCount2) {
     document.querySelector("h2").innerHTML = "Tie";
   } else {
     alert("Refresh the page there is a error");
   }
+
 });
